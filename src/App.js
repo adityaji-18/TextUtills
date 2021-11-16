318b989ecc0d381754e3ee55d2ac2d067fb8dab8
Aditya Sharma <adityaji@gmail.com>
2021-11-16 23:13:33 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import Navbar from './components/Navbar';
+import './App.css';
+import TextFom from './components/TextFom';
+import { useState } from 'react/cjs/react.development';
+// import About from './components/About';
+// import Alert from './components/Alert';
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-router-dom";
+
+
+
+
+function App() {
+  const [mode, setMode]= useState('light');
+  const toggleMode =() =>{
+    if(mode === 'light'){
+        setMode('dark');
+        document.title="TextUtills Dark Mode";
+      }
+      else{
+        setMode('light ');
+        document.title="TextUtills Light Mode";
+      }
+  }
+
+  return (
+    <>
+    {/* <Router> */}
+    <Navbar title="Textutiils" about="About" toggleMode= {toggleMode} mode={mode} />
+    <div className="container my-3">
+    {/* <Switch> */}
+        
+          {/* <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/">
+            
+          </Route>
+          
+    </Switch> */}
+    <TextFom heading="Enter The Text To Analyze:" mode={mode}/>
+    </div>
+    {/* </Router> */}
+   </>
+    
+  );
+}
+export default App;
